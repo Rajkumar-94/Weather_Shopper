6248800e01da4a809c632ac453688722fdbf3e23
Rajkumar M <rajkumar.m@qxf2.com>
2019-12-20 20:08:34 +0530

    Select Moisturizer or Sunscreen based on climate

--- read_weather_and_add_item_to_the_cart.py
+++ read_weather_and_add_item_to_the_cart.py
@@ -4,14 +4,17 @@ import time
 def launch_webpage():
     global driver
     driver = webdriver.Chrome()
-    # Maximize the browser window
     driver.maximize_window()
-    # Navigate to Qxf2 Tutorial page
     driver.get("https://weathershopper.pythonanywhere.com/")
     time.sleep(3)
+    
+def get_climate_temperature():    
     temperature=driver.find_element_by_xpath("//span[@id='temperature']").text
+    global temp
     temp=int(temperature.split()[0])
     print("The Temperature is: ",temp)
+
+def choose_product_based_on_climate():
     if temp<19:
         driver.find_element_by_xpath("//button[text()='Buy moisturizers']").click()
         time.sleep(2)
@@ -28,6 +31,12 @@ def launch_webpage():
             print("Successly navigated to Sunscreens page based on the climate")
         else:
             print("Fail:Did not navigated to Sunscreens page")
-    
 
-launch_webpage()
+def close_webpage():
+    driver.quit()
+
+if __name__ == "__main__":
+    launch_webpage()
+    get_climate_temperature()
+    choose_product_based_on_climate()
+    close_webpage()
